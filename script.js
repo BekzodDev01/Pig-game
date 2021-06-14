d476a5df7dddd6b7518db4a3680c85dcc00567b8
Zubayr-dev <bekzodbekolimbekov@gmail.com>
2021-06-14 17:52:56 +0500

    finished project using Js DOM

--- script.js
+++ script.js
@@ -1,52 +1,79 @@
-let scores, roundScores, activePlayer, dice;
-scores=[0,0];
-roundScores=0;
-activePlayer=0;
+let scores, roundScores, activePlayer, dice, isGamePlaying;
 
-document.querySelector('.dice').style.display='none'        
-document.getElementById('score--0').textContent='0';        
-document.getElementById('current--0').textContent='0';          
-document.getElementById('score--1').textContent='0';                  
-document.getElementById('current--1').textContent='0';
+init();
+
 document.querySelector('.btn--roll').addEventListener('click', function(){      
-    
-    let diceDOM= document.querySelector('.dice');           
-    dice= Math.floor(Math.random()*6+1);            
-    diceDOM.style.display='block';              
-    diceDOM.src='dice-'+dice+'.png';                
+    if (isGamePlaying){
 
+        let diceDOM= document.querySelector('.dice');           
+        dice= Math.floor(Math.random()*6+1);            
+        diceDOM.style.display='block';              
+        diceDOM.src='dice-'+dice+'.png';                
     
-    if (dice!==1){          
-        roundScores+=dice;
-        document.querySelector('#current--'+ activePlayer).textContent=roundScores;
-    }  
-    else {
-        nextPlayer();
+        
+        if (dice!==1){          
+            roundScores+=dice;
+            document.querySelector('#current--'+ activePlayer).textContent=roundScores;
+        }  
+        else {
+            nextPlayer();
+        }
+
+
     }
+
+
 });
 
     document.querySelector('.btn--hold').addEventListener('click', function(){
-        // Adding current score to golbal score 
+                if(isGamePlaying){
+
+            // Adding current score to golbal score 
         scores[activePlayer]+=roundScores;
         
         //Changing the score UI
     (document.getElementById('score--'+activePlayer).textContent=scores[activePlayer]);   
         if (scores[activePlayer] >= 20){
-                document.getElementById('name--1').textContent='Winner !';
-           document.querySelector('.player-' +activePlayer+'-1').classList.add('winner')
-           document.querySelector('.player-' +activePlayer+'-1').classList.remove('player--active')
+           document.getElementById('name--'+activePlayer).textContent='Winner !';
+           document.querySelector('.player--' +activePlayer).classList.add('player--winner');
+           document.querySelector('.player--' +activePlayer).classList.remove('player--active');
+           isGamePlaying=false
         }
         
         else {
             nextPlayer();
-        }
+        }           
+                }
+
+     
 
         
 
 
 });
 
+document.querySelector('.btn--new').addEventListener('click', init);
 
+function init(){
+    scores=[0,0];
+    roundScores=0;
+    activePlayer=0;
+    isGamePlaying= true;    
+    
+    document.querySelector('.dice').style.display='none'        
+    document.getElementById('score--0').textContent='0';        
+    document.getElementById('current--0').textContent='0';          
+    document.getElementById('score--1').textContent='0';                  
+    document.getElementById('current--1').textContent='0';
+    document.getElementById('name--0').textContent='Player 1';
+    document.getElementById('name--1').textContent='Player 2';
+    document.querySelector('.player--0').classList.remove('player--winner');
+    document.querySelector('.player--0').classList.remove('player--active');
+    document.querySelector('.player--1').classList.remove('player--active');
+    document.querySelector('.player--1').classList.remove('player--winner');
+    document.querySelector('.player--0').classList.add('player--active');
+
+}
 
 function nextPlayer(){
     activePlayer ===0 ? activePlayer=1: activePlayer=0
