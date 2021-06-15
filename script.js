b3abd40261eccc8a61951dd7434cd441277447ab
Zubayr-dev <bekzodbekolimbekov@gmail.com>
2021-06-15 12:33:10 +0500

    Project finished with new features

--- script.js
+++ script.js
@@ -1,59 +1,72 @@
-let scores, roundScores, activePlayer, dice, isGamePlaying;
+let scores, roundScores, activePlayer, dice, isGamePlaying, lastDice;
 
+let winnerScore;
 init();
 
 document.querySelector('.btn--roll').addEventListener('click', function(){      
     if (isGamePlaying){
-
         let diceDOM= document.querySelector('.dice');           
-        dice= Math.floor(Math.random()*6+1);            
+        count=0;
+        dice= Math.floor(Math.random()*6+1);   
+        
+
         diceDOM.style.display='block';              
         diceDOM.src='dice-'+dice+'.png';                
-    
-        
-        if (dice!==1){          
+
+        if (dice===6 && lastDice===6){
+            scores[activePlayer]=0;
+            document.getElementById('score--'+activePlayer).textContent='0';
+            console.log(document.getElementById('score--'+activePlayer).textContent);
+            nextPlayer();
+        }
+
+       else if (dice!==1){          
             roundScores+=dice;
-            document.querySelector('#current--'+ activePlayer).textContent=roundScores;
+             document.querySelector('#current--'+ activePlayer).textContent=roundScores;
+           
         }  
         else {
             nextPlayer();
         }
-
+        lastDice=dice;
 
     }
 
 
 });
-
     document.querySelector('.btn--hold').addEventListener('click', function(){
                 if(isGamePlaying){
 
             // Adding current score to golbal score 
         scores[activePlayer]+=roundScores;
-        
+                
         //Changing the score UI
     (document.getElementById('score--'+activePlayer).textContent=scores[activePlayer]);   
-        if (scores[activePlayer] >= 20){
+          let input=document.querySelector('.finalScore').value;
+                    if(input){
+                        winnerScore=input;
+                    }
+                    else{
+                        winnerScore=100;
+                    }
+          if (scores[activePlayer] >= winnerScore){
            document.getElementById('name--'+activePlayer).textContent='Winner !';
            document.querySelector('.player--' +activePlayer).classList.add('player--winner');
            document.querySelector('.player--' +activePlayer).classList.remove('player--active');
            isGamePlaying=false
+
         }
-        
         else {
             nextPlayer();
         }           
-                }
-
-     
 
         
 
-
+                }
 });
 
-document.querySelector('.btn--new').addEventListener('click', init);
 
+document.querySelector('.btn--new').addEventListener('click', init);
 function init(){
     scores=[0,0];
     roundScores=0;
@@ -72,7 +85,7 @@ function init(){
     document.querySelector('.player--1').classList.remove('player--active');
     document.querySelector('.player--1').classList.remove('player--winner');
     document.querySelector('.player--0').classList.add('player--active');
-
+        
 }
 
 function nextPlayer(){
